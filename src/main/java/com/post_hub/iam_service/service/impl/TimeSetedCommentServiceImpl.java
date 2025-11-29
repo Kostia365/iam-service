b288f3d1a5329d950c3598c2c295b895b2607862
Kostia365 <90925004+Kostia365@users.noreply.github.com>
2025-11-29 15:26:20 +0100

    feat: comments comments wwith time

---
+++ src/main/java/com/post_hub/iam_service/service/impl/TimeSetedCommentServiceImpl.java
@@ -0,0 +1,18 @@
+package com.post_hub.iam_service.service.impl;
+
+import com.post_hub.iam_service.service.CommentService;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeSetedCommentServiceImpl implements CommentService {
+  private final List<String> comments = new ArrayList<>();
+
+  @Override
+  public void createComment(String commentContent) {
+    String advancedComment = "[" + LocalDateTime.now() + "] " + commentContent.toUpperCase();
+    comments.add(commentContent);
+    System.out.println("Advanced comment: " + advancedComment);
+  }
+}
