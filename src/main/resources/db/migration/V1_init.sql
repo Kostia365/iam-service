ee288f9623519e53c90eac1a55984ba70c70f77a
Kostia365 <90925004+Kostia365@users.noreply.github.com>
2025-12-20 13:24:47 +0100

    feat: first query

--- src/main/resources/db/migration/V1_init.sql
+++ src/main/resources/db/migration/V1_init.sql
@@ -1,1 +1,12 @@
-SELECT *
+CREATE table posts
+(
+    id         bigserial primary key,
+    title      varchar(255) not null,
+    content    text         not null,
+    created_at timestamp    not null default current_timestamp,
+    likes      integer      not null default 0,
+    unique (title)
+);
+insert into posts (title, content, created_at, likes)
+values ('First post', 'This is my first post', current_timestamp, 0),
+       ('Second post', 'This is my second post', current_timestamp, 10);
